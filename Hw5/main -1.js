fffd93e8e6882c930a4f580ec0a593267ee0d493
簡珮軒 <113080276+cpeggy@users.noreply.github.com>
2023-05-31 13:46:05 +0800

    final

--- Hw5/main -1.js
+++ Hw5/main -1.js
@@ -14,7 +14,7 @@ function processImageFile(imageObject) {
 
     var params = {
         "visualFeatures": "Categories,Objects,Color",
-        "details":"",
+        "details": "",
         "language": "en",
     };
 
@@ -38,7 +38,7 @@ function processImageFile(imageObject) {
         $("#responseTextArea").val(JSON.stringify(data, null, 2));
         $("#picDescription").empty();
 
-        $("#picDescription").append("Color: " + data.color.dominantColors + "<br>");
+        $("#picDescription").append("Main Color: " + data.color.dominantColorForeground + "<br>");
         $("#picDescription").append("Object: " + data.objects[0].object + "<br>");
     })
     .fail(function (jqXHR, textStatus, errorThrown) {
@@ -49,44 +49,37 @@ function processImageFile(imageObject) {
 };
 
 function processImage() {
-    
-    //確認區域與所選擇的相同或使用客製化端點網址
     var url = "https://eastus.api.cognitive.microsoft.com/";
     var uriBase = url + "vision/v2.1/analyze";
-    
+
     var params = {
         "visualFeatures": "Categories,Objects,Color",
-        "details":"",
+        "details": "",
         "language": "en",
     };
-    //顯示分析的圖片
+
     var sourceImageUrl = document.getElementById("inputImage").value;
     document.querySelector("#sourceImage").src = sourceImageUrl;
-    //送出分析
+
     $.ajax({
         url: uriBase + "?" + $.param(params),
-        // Request header
         beforeSend: function(xhrObj){
             xhrObj.setRequestHeader("Content-Type","application/json");
             xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
         },
         type: "POST",
-        // Request body
-        data: '{"url": ' + '"' + sourceImageUrl + '"}',
+        data: '{"url": "' + sourceImageUrl + '"}',
     })
     .done(function(data) {
-        //顯示JSON內容
         $("#responseTextArea").val(JSON.stringify(data, null, 2));
         $("#picDescription").empty();
         
-        $("#picDescription").append("Color: "+data.color.dominantColors+"<br>");
-        $("#picDescription").append("Object: "+data.objects[0].object+"<br>");
+        $("#picDescription").append("Main Color: " + data.color.dominantColorForeground + "<br>");
+        $("#picDescription").append("Object: " + data.objects[0].object + "<br>");
     })
     .fail(function(jqXHR, textStatus, errorThrown) {
-        //丟出錯誤訊息
         var errorString = (errorThrown === "") ? "Error. " : errorThrown + " (" + jqXHR.status + "): ";
         errorString += (jqXHR.responseText === "") ? "" : jQuery.parseJSON(jqXHR.responseText).message;
         alert(errorString);
     });
 };
-
